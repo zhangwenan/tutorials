7f46de36424d4196358e515a0009031251869f66
zhangwenan <jquerytech@gmail.com>
2018-04-16 21:02:07 +0800

    创建相关的reducers

---
+++ examples/redux/demo4-todos-with-undo/src/reducers/index.js
@@ -0,0 +1,11 @@
+import { combineReducers } from 'redux';
+
+import todos from './todos';
+import visibilityFilters from './visibilityFilters';
+
+const todoApp = combineReducers({
+    todos,
+    visibilityFilters
+})
+
+export default todoApp;
